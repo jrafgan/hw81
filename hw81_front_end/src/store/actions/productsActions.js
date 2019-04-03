aa2ed3cc08b8c6fef259eea875644d5f07ea8d98
Maksat Duishenaliev <maksatovy@gmail.com>
2019-04-03 16:51:38 +0600

    refactoring made

---
+++ hw81_front_end/src/store/actions/productsActions.js
@@ -0,0 +1,26 @@
+import axios from '../../axios-api';
+
+export const FETCH_PRODUCTS_SUCCESS = 'FETCH_PRODUCTS_SUCCESS';
+export const CREATE_PRODUCT_SUCCESS = 'CREATE_PRODUCT_SUCCESS';
+
+export const fetchProductsSuccess = products => ({type: FETCH_PRODUCTS_SUCCESS, products});
+export const createProductSuccess = () => ({type: CREATE_PRODUCT_SUCCESS});
+
+export const fetchProducts = () => {
+    return dispatch => {
+        return axios.get('/products').then(
+            response => dispatch(fetchProductsSuccess(response.data))
+        );
+    }
+};
+
+export const createProduct = productData => {
+    return dispatch => {
+        return axios.post('/products', productData).then(
+            () => {
+
+        console.log(productData);
+        dispatch(createProductSuccess());
+    });
+};
+};
